21e74a15d6c0cba83931d01b9e83384beda4e5b1
Julia Loy <julialoy@gmail.com>
2020-03-13 15:23:45 -0400

    Initial commit

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,20 @@
+/*
+*
+*
+*       FILL IN EACH UNIT TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]----
+*       (if additional are added, keep them at the very end!)
+*/
+
+const chai = require('chai');
+
+// Commented out. File not needed/does not exist.
+// const StockHandler = require('../controllers/stockHandler.js');
+
+// const stockPrices = new StockHandler();
+
+suite('Unit Tests', function(){
+
+//none required
+
+});
